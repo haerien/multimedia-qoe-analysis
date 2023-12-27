17892a91616d9cc3be814d666a21e6aa89e1da4c
Emre Sahin <sahinemre.dev@gmail.com>
2023-12-28 00:45:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.post('/api', (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(process.env.PORT || PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });

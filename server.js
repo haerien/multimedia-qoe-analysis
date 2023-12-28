6ce921418f0e79e08fc01928343a7be34e684744
Emre Sahin <sahinemre.dev@gmail.com>
2023-12-28 12:29:22 +0300

    Testing

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
-//app.use(express.json());
-var distDir = "./node_modules/express";
-app.use(express.static(distDir));
+app.use(express.json());
 app.use(cors());
 /*const corsOptions = {
     origin: 'https://haerien.github.io',

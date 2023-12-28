012c3ab798ac39412fea151b8002eb62bdd1bab8
Emre Sahin <sahinemre.dev@gmail.com>
2023-12-28 12:41:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
-const PORT = process.env.PORT || 5001
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.json());
+app.use(cors());
+/*const corsOptions = {
+    origin: 'https://haerien.github.io',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+};*/
+//app.use(cors(corsOptions));
+
+const PORT = 3000 || process.env.PORT;
+
+app.use(bodyParser.json());
+
+// MongoDB Connection
+const mongoURI = 'mongodb+srv://admin:admin@cluster0.1e9ebol.mongodb.net/?retryWrites=true&w=majority';
+//const mongoURI = 'mongodb://localhost:27017';
+const dbName = 'api';
+let db;
+
+MongoClient.connect(mongoURI, {serverSelectionTimeoutMS: 5000})
+  .then(client => {
+    console.log('Connected to MongoDB');
+    db = client.db(dbName);
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+// Endpoint to store QoS parameters
+app.post('/api', (req, res) => {
+  const qosData = req.body; // Data sent from the client
+  // Store data in MongoDB
+  db.collection('qosData').insertOne(qosData, (err, result) => {
+    if (err) return res.status(500).send('Error storing QoS parameters');
+    res.status(200).send('QoS parameters stored successfully');
+  });
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(process.env.PORT || PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
